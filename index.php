e8044268e72495addb0e96b680a42a568723d1ee
netgigs <netgigs101@gmail.com>
2015-01-29 13:33:52 +0100

    ...and this is where we start from

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+
+
+<link rel="stylesheet" type="text/css" href="basic.css" />
+<link rel="stylesheet" type="text/css" href="basic2.css" />
+
+
+
+
+
+
+
+
+
+
+
+
+
+<title>Untitled Document</title>
+</head>
+
+
+
+<body>
+
+
+
+
+
+<div class="container" id="container">
+
+    
+	<div class="header" id="header">
+	
+	    <div class="tittlename" id="tittlename">
+		 <span id="tittlecolor"> Adashe</span>.<span id="tittlecolor2">ng</span>
+		</div>  
+		  
+	</div>
+	
+	
+	<div id="menucontainer">
+	
+	  <div id="menuholder">
+	    <div id="home">Home</div>
+		<div id="aboutus"> About us</div>
+		<div id="howitworks"> How it works</div>
+		<div id="termsandconditions"> Terms and conditions</div>
+		<div id="contactus"> Contact us</div>
+	  </div>
+	
+	</div>
+	
+	<div id="categories">
+	
+		        <ul class="menu2" id="menu2" >
+					   
+					   <li class="cat" id="cat" align="" >
+			
+			                <a href="#">Categories &darr;&darr; </a>
+				      
+					        <ul  class="cate" id="cate" align="center"  style="text-align:;">
+							
+							   <a href="#"><li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bronze</li></a>
+							   <a href="#"><li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;silver</li></a>
+							   <a href="#"><li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;gold</li></a>
+							   <a href="#"><li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;diamon</li></a>
+					                             
+						    </ul>
+					  
+			          </li>
+					   
+				</ul>	
+	
+	</div>
+	
+	<br />
+	<div id="loginform">
+	  <br />
+	  <div id="form">
+	  
+	  	  <form id="formitself" class="formitself" action="login.php" method="post" class="login">
+		  
+		  
+		  <div id="formtittle1" class="formtittle1"> Enter Your Account</div>
+		  
+			<br />
+                 <input type="text" name="email" value=" Email"  id="input1" class="input1" /> 
+				<br /><br />
+			     <input type="password" name="password" value="password" id="input2" class="input2"  /> 
+				<br /><br />
+				<input type="submit" value="login" name="submit"  id="input3" class="input3" />
+				
+				<div id="forgetpassword">forgot your password ?</div>
+				
+		    <hr style="color: #F4F4F4; width:250px;" />	
+			
+			<div id="notecreataccount">
+			
+			If you are new here, click below to creat an account.. 
+			  
+			  <div id="creataccountbotton"> CREAT ACCOUNT</div>
+			
+			</div>	
+
+		  </form>
+	  
+	  </div>
+	  
+	  
+	  <div id="welcomenote" style=" ">
+	  
+	     <div id="wotadashe" style="">
+		   <span id="" style="font-size: 36px; color: #009797; margin-left:25px; font-stretch:extra-expanded;">WHAT IS ADASHE ?</span><br /><br />
+		    
+			The english term for adashe is thrift, which is defined as a way saving money in a cooporative manner.
+			A group of individuals, come together, put some part of their montly salaries into a separate account where each of the paticipant
+			would take turn in witdrawing the whole amount saved in that account for that month, acording to the month he or she picks.<br /> 
+			<span style="color:#00C4C4; text-align:right;">... read more.</span>  
+			
+		 </div><br /><br />
+		 
+		 <div id="whyadashe">
+		  <span id="" style="font-size: 36px; color: #009797; margin-left:25px; font-stretch:extra-expanded;"> WHY DO ADASHE ?</span><br /><br /> 
+		  
+			The english term for adashe is thrift, which is defined as a way saving money in a cooporative manner.
+			A group of individuals, come together, put some part of their montly salaries into a separate account where each of the paticipant
+			would take turn in witdrawing the whole amount saved in that account for that month, acording to the month he or she picks.<br /> 
+			<span style="color:#00C4C4; text-align:right;">... read more.</span>		  
+		 </div>
+	  	  
+	  
+	  </div>
+	  
+	
+	</div>
+	
+	<br /><br />
+	
+	<div id="typesofadashe">
+	 <span style="font-size: 36px; color: #009797; margin-left:25px; font-stretch:extra-expanded;">types of adashe</span>
+	 <hr style="color: #F5F5F5;" /> 
+	  
+	  <div id="fivet" align="center"><br />
+	     5,000 <br />
+		 naira <br />
+		 MONTHLY<br /><br />
+         <div id="enteradashe">Enter Adashe</div><br />
+		 
+	  </div>
+	  <div id="tent" align="center"><br />
+	     10,000 <br />
+		 naira <br />
+		 MONTHLY<br /><br />
+         <div id="enteradashe">Enter Adashe</div><br />
+		 	  
+	  </div>
+	  <div id="thirtyt" align="center"><br />
+	     30,000 <br />
+		 naira <br />
+		 MONTHLY<br /><br />
+         <div id="enteradashe">Enter Adashe</div><br />	  
+	  
+	  </div>
+	  <div id="fiftyt" align="center"><br />
+	     50,000 <br />
+		 naira <br />
+		 MONTHLY<br /><br />
+         <div id="enteradashe">Enter Adashe</div><br />	  
+	  
+	  </div>
+	  <div id="hundredt" align="center"><br />
+	     100,000 <br />
+		 naira <br />
+		 MONTHLY<br /><br />
+         <div id="enteradashe">Enter Adashe</div><br />	  
+	  
+	  </div>
+	  
+	  <br />
+	</div>
+
+	<div id="divide"></div>
+    <div id="footer"></div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
